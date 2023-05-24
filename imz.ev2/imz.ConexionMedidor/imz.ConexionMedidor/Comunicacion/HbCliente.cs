ecc981101bd9129bf77e689cacd82634aa069218
Agustin Muñoz <112513840+Agustinimz@users.noreply.github.com>
2023-05-23 20:35:16 -0400

    Medidor Eléctrico

---
+++ imz.ev2/imz.ConexionMedidor/imz.ConexionMedidor/Comunicacion/HbCliente.cs
@@ -0,0 +1,48 @@
+﻿using ModeloLectura;
+using ModeloLectura.DAL;
+using ServidorSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imz.ConexionMedidor.Comunicacion
+{
+    public class HbCliente
+    {
+        private ClienteCom clienteCom;
+        private ModeloLecturaDAL mensajesDAL = ModeloLecturaDALArchivos.GetInstancia();
+
+        public HbCliente(ClienteCom clienteCom)
+        {
+            this.clienteCom = clienteCom;
+        }
+
+        public void ejecutar()
+        {
+            //ahora traemos el codigo que atiende al cliente
+            clienteCom.Escribir("Ingrese Numero de Medidor (formato 1234567)");
+            string nromedidor = clienteCom.Leer();
+
+            clienteCom.Escribir("Ingrese Fecha (formato yyyy-MM-dd HH:mm:ss)");
+            string fecha = clienteCom.Leer();
+
+            clienteCom.Escribir("Ingrese Valor Consumo (kw/h utilizando coma decimal)");
+            string valorconsumo = clienteCom.Leer();
+            LecturaMedidor lecturamedidor = new LecturaMedidor()
+            {
+                NroMedidor = Convert.ToInt32(nromedidor),
+                Fecha = fecha,
+                ValorConsumo = Convert.ToDecimal(valorconsumo)
+            };
+            lock (mensajesDAL)
+            {
+                mensajesDAL.AgregarMensaje(lecturamedidor);
+            }
+
+            clienteCom.Escribir("chao");
+            clienteCom.Desconectar();
+        }
+    }
+}
---
+++ imz.ev2/imz.ConexionMedidor/imz.ConexionMedidor/Comunicacion/HbServidor.cs
@@ -0,0 +1,49 @@
+﻿using ModeloLectura.DAL;
+using ServidorSocket;
+using System;
+using System.Configuration;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace imz.ConexionMedidor.Comunicacion
+{
+    public class HbServidor
+    {
+        private ModeloLecturaDAL mensajesDAL = ModeloLecturaDALArchivos.GetInstancia();
+        public void Ejecutar()
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket servidor = new ServerSocket(puerto);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("S: Servidor iniciado en puerto {0}", puerto);
+            Console.ResetColor();
+            if (servidor.Iniciar())
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("                              S: Esperando cliente....");
+                    Console.ResetColor();
+                    Socket cliente = servidor.ObtenerCliente();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("                              S: Cliente recibido");
+                    Console.ResetColor();
+                    ClienteCom clienteCom = new ClienteCom(cliente);
+
+                    HbCliente clienteThread = new HbCliente(clienteCom);
+                    Thread t = new Thread(new ThreadStart(clienteThread.ejecutar));
+                    t.IsBackground = true;
+                    t.Start();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Error fatal {0}", puerto);
+                Console.ResetColor();
+            }
+        }
+    }
+}
